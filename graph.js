f152c42716413ceaae7be75b013f6eb0ce7fa616
LevshinMisha <LevshinMisha@gmail.com>
2017-11-11 22:42:13 +0500

    Рефакторинг - вдруг заработает

--- graph.js
+++ graph.js
@@ -4,19 +4,14 @@ class Node {
     constructor(value) {
         this.value = value;
         this.name = value.name;
-        this.deep = 9999999999999;
+        this.deep = Infinity;
         this.connected = [];
+        this.visited = false;
     }
 
     connect(node) {
         if (!(node in this.connected)) {
             this.connected.push(node);
-        }
-    }
-
-    disconnect(node) {
-        if (node in this.connected) {
-            this.connected.remove(node);
         }
     }
 }
@@ -34,67 +29,41 @@ let compareNode = (node1, node2) => {
 
 module.exports = class {
     constructor(friends, filter) {
-        this.nodes = [];
-        friends.forEach(friend => {
-            this.nodes.push(new Node(friend));
-        });
+        this.nodes = friends.map(friend => new Node(friend));
         this.connectNodes();
-        this.setDeeps(this.nodes.filter(node => node.value.best), 0, []);
+        this.setDeeps(this.nodes.filter(node => node.value.best), 0);
         this.filter(node => filter.func(node.value));
     }
 
     connectNodes() {
         this.nodes.forEach(node => {
-            node.value.friends.forEach(friend => {
-                let friendNode = this.getNode(friend);
-                if (friendNode !== null) {
-                    node.connect(friendNode);
-                }
-            });
+            node.value.friends.forEach(name => node.connect(this.nodes.find(node2 => {
+                return node2.value.name === name;
+            })));
         });
     }
 
-    setDeeps(nodes, deep, visited) {
+    setDeeps(nodes, deep) {
         if (nodes.length === 0) {
             return;
         }
-        let newNodes = [];
+        let newNodes = new Set();
         nodes.forEach(node => {
-            if (!(node in visited)) {
-                visited.push(node);
-                if (node.deep > deep) {
-                    node.deep = deep;
-                    node.connected.forEach(i => {
-                        if (!newNodes.includes(i) && !visited.includes(i)) {
-                            newNodes.push(i);
-                        }
-                    });
-                }
+            if (!node.visited) {
+                node.visited = true;
+                node.deep = deep;
+                node.connected.forEach(i => {
+                    if (!i.visited) {
+                        newNodes.add(i);
+                    }
+                });
             }
         });
-        this.setDeeps(newNodes, deep + 1, visited);
-    }
-
-    getNode(name) {
-        for (let i = 0; i < this.nodes.length; i++) {
-            if (this.nodes[i].value.name === name) {
-                return this.nodes[i];
-            }
-        }
-
-        return null;
+        this.setDeeps([...newNodes].sort(compareNode), deep + 1);
     }
 
     filter(func) {
-        this.nodes = this.nodes.filter(func);
-        this.nodes.forEach(node => {
-            node.connected.forEach(node2 => {
-                if (!(node2 in this.nodes)) {
-                    node.disconnect(node2);
-                }
-            });
-        });
-        this.nodes = this.nodes.sort(compareNode);
+        this.nodes = this.nodes.filter(func).sort(compareNode);
     }
 
     next() {
