d0ffcbd88cff33ccb58999c5226935421e2b72d9
Anton <graf.m.96@yandex.ru>
2017-11-11 20:40:02 +0500

    first attempt

--- lib.js
+++ lib.js
@@ -1,5 +1,66 @@
 'use strict';
 
+
+exports.isStar = true;
+
+
+/**
+ * @param {Array<name, friends, gender, best>} friends
+ * @param {Filter} filter
+ * @param {Number} maxLevel
+ * @returns {Array<name, friends, gender, best>}
+ */
+function getEnumerator(friends, filter, maxLevel = Infinity) {
+    let nameFriendToCircleNumber = calculateCircleNumbers(friends);
+
+    return filter.filter(friends)
+        .filter(friend => friend.name in nameFriendToCircleNumber &&
+                nameFriendToCircleNumber[friend.name] <= maxLevel)
+        .sort((thisFriend, otherFriend) => {
+            if (nameFriendToCircleNumber[thisFriend.name] <
+                nameFriendToCircleNumber[otherFriend.name]) {
+                return - 1;
+            }
+            if (nameFriendToCircleNumber[thisFriend.name] >
+                nameFriendToCircleNumber[otherFriend.name]) {
+                return 1;
+            }
+
+            return thisFriend.name <= otherFriend.name ? -1 : 1;
+        });
+}
+
+
+/**
+ * @param {Array<name, friends, gender, best>} friends
+ * @returns {Object}
+ */
+function calculateCircleNumbers(friends) {
+    let nameToFriend = friends.reduce((accumulator, friend) => {
+        accumulator[friend.name] = friend;
+
+        return accumulator;
+    }, {});
+    let queueOfFriend = friends.filter(friend => friend.best);
+    let nameFriendToCircleNumber = queueOfFriend.reduce((accumulator, friend) => {
+        accumulator[friend.name] = 1;
+
+        return accumulator;
+    }, {});
+    for (let i = 0; i < queueOfFriend.length; i++) {
+        let friend = queueOfFriend[i];
+        friend.friends.forEach(nameOtherFriend => {
+            if (nameOtherFriend in nameFriendToCircleNumber) {
+                return;
+            }
+            nameFriendToCircleNumber[nameOtherFriend] = nameFriendToCircleNumber[friend.name] + 1;
+            queueOfFriend.push(nameToFriend[nameOtherFriend]);
+        });
+    }
+
+    return nameFriendToCircleNumber;
+}
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -7,8 +68,30 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    this.checkFilterType(filter);
+    this.enumerator = getEnumerator(friends, filter);
+    this.index = 0;
 }
+
+Object.assign(Iterator.prototype, {
+    checkFilterType(filter) {
+        if (!(filter instanceof Filter)) {
+            throw new TypeError('Конструктор Iterator должен принимать filter, ' +
+                                'который имеет в прототипах Filter');
+        }
+    },
+    done() {
+        return this.index >= this.enumerator.length;
+    },
+    next() {
+        if (this.done()) {
+            return null;
+        }
+        this.index++;
+
+        return this.enumerator[this.index - 1];
+    }
+});
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -19,16 +102,26 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    this.checkFilterType(filter);
+    this.enumerator = getEnumerator(friends, filter, maxLevel);
+    this.index = 0;
 }
+
+Object.setPrototypeOf(LimitedIterator.prototype, Iterator.prototype);
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.selectionCondition = () => true;
 }
+
+Object.assign(Filter.prototype, {
+    filter(friends) {
+        return friends.filter(friend => this.selectionCondition(friend));
+    }
+});
 
 /**
  * Фильтр друзей
@@ -36,8 +129,10 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.selectionCondition = friend => friend.gender === 'male';
 }
+
+Object.setPrototypeOf(MaleFilter.prototype, Filter.prototype);
 
 /**
  * Фильтр друзей-девушек
@@ -45,8 +140,10 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.selectionCondition = friend => friend.gender === 'female';
 }
+
+Object.setPrototypeOf(FemaleFilter.prototype, Filter.prototype);
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
