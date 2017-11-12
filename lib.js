651e6cf7996e0f208cad8ac2740afb669807c64c
ADruzhinin <mcfriend@yandex.ru>
2017-11-12 13:42:22 +0500

    first try

--- lib.js
+++ lib.js
@@ -1,5 +1,65 @@
 'use strict';
 
+function compareNames(a, b) {
+    return a.name > b.name ? 1 : -1;
+}
+
+function makeFriendsByName(friends) {
+    return friends.reduce((object, currentFriend) => {
+        object[currentFriend.name] = currentFriend;
+
+        return object;
+    }, {});
+}
+
+function fillingLevel(invited, person, objectForFriends, nextLevel) {
+    invited.push(person);
+    delete objectForFriends[person.name];
+    person.friends.forEach(friend => {
+        if (friend in objectForFriends) {
+            nextLevel.push(objectForFriends[friend]);
+        }
+    });
+}
+
+function fillingLevels(invited, nextLevel, maxLevel, objectForFriends) {
+    let currentLevel = nextLevel;
+    nextLevel = [];
+    let fillingFriends = person => {
+        if (person.name in objectForFriends) {
+            fillingLevel(invited, person, objectForFriends, nextLevel);
+        }
+    };
+    while (currentLevel.length !== 0 && maxLevel !== 0) {
+        currentLevel.sort(compareNames);
+        currentLevel.forEach(fillingFriends);
+        currentLevel = nextLevel;
+        nextLevel = [];
+        maxLevel--;
+    }
+
+    return invited;
+}
+
+function distributeByLevels(friends, filter, maxLevel) {
+    if (!friends || !maxLevel || maxLevel < 1) {
+        return [];
+    }
+    let invited = [];
+    let nextLevel = [];
+    let objectForFriends = makeFriendsByName(friends);
+    friends.sort(compareNames).forEach(person => {
+        if (person.best) {
+            fillingLevel(invited, person, objectForFriends, nextLevel);
+        }
+    });
+    maxLevel--;
+    invited = filter.getPeopleWithSameGender(
+        fillingLevels(invited, nextLevel, maxLevel, objectForFriends));
+
+    return invited;
+}
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -7,8 +67,24 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError('filter не является объектом конструктора Filter');
+    }
+    this.friends = distributeByLevels(friends, filter, Infinity);
+    this.indexCurrentFriend = 0;
 }
+
+Iterator.prototype.done = function () {
+    return this.indexCurrentFriend === this.friends.length;
+};
+Iterator.prototype.next = function () {
+    if (this.done()) {
+        return null;
+    }
+
+    return this.friends[this.indexCurrentFriend++];
+};
+
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -19,16 +95,25 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    Iterator.call(this, friends, filter);
+    this.friends = distributeByLevels(friends, filter, maxLevel);
 }
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.gender = 'nothing';
 }
+
+Filter.prototype.getPeopleWithSameGender = function (people) {
+    return people.filter(function (person) {
+        return person.gender === this.gender || this.gender === 'nothing';
+    }, this);
+};
 
 /**
  * Фильтр друзей
@@ -36,8 +121,11 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.gender = 'male';
 }
+
+MaleFilter.prototype = Object.create(Filter.prototype);
+
 
 /**
  * Фильтр друзей-девушек
@@ -45,8 +133,10 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.gender = 'female';
 }
+
+FemaleFilter.prototype = Object.create(Filter.prototype);
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
