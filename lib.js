88fa8b0ef0b66906af859d271e3c11f8f4b44aee
Evgeny Markov <evgenymarkov@yandex.ru>
2017-11-12 18:12:09 +0500

    First attempt

--- lib.js
+++ lib.js
@@ -1,5 +1,42 @@
 'use strict';
 
+let friendsSortRule = (first, second) => first.name.localeCompare(second.name);
+
+/**
+ * Подготовка списка друзей, которых Билли пригласит на свадьбу.
+ * @param {Object[]} friends – список друзей из блокнота Билли.
+ * @param {Filter} filter – фильтр для друзей.
+ * @param {Number} maxLevel – максимальный круг друзей.
+ * @returns {Object[]} – отфильрованный и отсортированный список приглашаемых друзей.
+ */
+function collectInvitedFriends(friends, filter, maxLevel = Infinity) {
+    let invitedFriends = [];
+
+    let currentLevelFriends = friends.filter(friend => friend.best).sort(friendsSortRule);
+    invitedFriends.splice(invitedFriends.length, 0, ...currentLevelFriends);
+
+    let currentLevelIndex = 1;
+    while (currentLevelIndex < maxLevel && invitedFriends.length !== friends.length) {
+        // 1. Собираем имена друзей следующего круга
+        // 2. Получаем объекты этих друзей
+        // 3. Оставляем только уникальных в списке и не приглашённых ранее
+        // 4. Сортируем друзей текущего круга в алфавитном порядке
+        let nextLevelFriends = currentLevelFriends
+            .reduce((acc, friend) => acc.concat(friend.friends), [])
+            .map(friendName => friends.find(friend => friend.name === friendName))
+            .filter((friend, pos, arr) => {
+                return !invitedFriends.includes(friend) && arr.indexOf(friend) === pos;
+            })
+            .sort(friendsSortRule);
+
+        currentLevelIndex++;
+        currentLevelFriends = nextLevelFriends;
+        invitedFriends.splice(invitedFriends.length, 0, ...currentLevelFriends);
+    }
+
+    return filter.filter(invitedFriends);
+}
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -7,8 +44,29 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError();
+    }
+
+    this.currentGuestIndex = 0;
+    this.invitedGuests = collectInvitedFriends(friends, filter);
 }
+
+/**
+ * Итерация по приглашаемым друзьям.
+ * @returns {null|Object} null если обход закончен иначе объект описывающий друга.
+ */
+Iterator.prototype.next = function () {
+    return this.done() ? null : this.invitedGuests[this.currentGuestIndex++];
+};
+
+/**
+ * Проверка завершения обхода.
+ * @returns {boolean} true, если обход закончен, и false в противном случае.
+ */
+Iterator.prototype.done = function () {
+    return this.currentGuestIndex === this.invitedGuests.length;
+};
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -19,16 +77,29 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    Object.getPrototypeOf(LimitedIterator.prototype).constructor.call(this, friends, filter);
+    this.invitedGuests = collectInvitedFriends(friends, filter, maxLevel);
 }
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+LimitedIterator.prototype.constructor = LimitedIterator;
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.condition = () => true;
 }
+
+/**
+ * Фильтрация приглашаемых гостей по заданному критерию.
+ * @param {Object[]} friends – список гостей.
+ * @returns {Object[]} – отфильтрованный по критерию список гостей.
+ */
+Filter.prototype.filter = function (friends) {
+    return friends.filter(this.condition);
+};
 
 /**
  * Фильтр друзей
@@ -36,8 +107,11 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.condition = friend => friend.gender === 'male';
 }
+
+MaleFilter.prototype = Object.create(Filter.prototype);
+MaleFilter.prototype.constructor = MaleFilter;
 
 /**
  * Фильтр друзей-девушек
@@ -45,8 +119,11 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.condition = friend => friend.gender === 'female';
 }
+
+FemaleFilter.prototype = Object.create(Filter.prototype);
+FemaleFilter.prototype.constructor = FemaleFilter;
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
