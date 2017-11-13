26519732c699ded17b20014f83a6eedd20c33c50
Ftlka <smart.Julia@yandex.ru>
2017-11-13 20:20:58 +0500

    Passes basic test

--- lib.js
+++ lib.js
@@ -1,52 +1,136 @@
 'use strict';
+const parents = [];
 
-/**
- * Итератор по друзьям
- * @constructor
- * @param {Object[]} friends
- * @param {Filter} filter
- */
+const findFriendByName = (name, friends) => {
+    for (let i = 0; i < friends.length; i ++) {
+        if (friends[i].name === name) {
+            return friends[i];
+        }
+    }
+};
+
+const findFriend = name => {
+    for (let i = 0; i < parents.length; i++) {
+        if (parents[i].name === name) {
+            return parents[i];
+        }
+    }
+};
+
+const getIdx = name => {
+    for (let i = 0; i < parents.length; i++) {
+        if (parents[i].name === name) {
+            return i;
+        }
+    }
+};
+
+const getParent = friend => {
+    if (friend && parents[getIdx(friend.name)]) {
+        return parents[getIdx(friend.name)].parent;
+    }
+
+    return null;
+};
+
+const countParents = friend => {
+    let counter = 1;
+    while (getParent(friend) !== null) {
+        counter++;
+        friend = findFriend(getParent(friend).name);
+    }
+
+    return counter;
+};
+
+const getLevel = (friend, friends) => {
+    const besties = friends.filter(element => element.best);
+    let queue = besties.slice();
+    let visited = besties.slice();
+
+    while (queue.length > 0) {
+        const current = queue.shift();
+        current.friends.forEach(element => {
+            let currentFriend = findFriendByName(element, friends);
+            if (!visited.includes(currentFriend)) {
+                parents.forEach(child => {
+                    if (child.name === currentFriend.name) {
+                        child.parent = current;
+                    }
+                });
+                queue.push(currentFriend);
+                visited.push(currentFriend);
+            }
+        });
+    }
+
+    return countParents(friend);
+};
+
+const sortByLevels = (friends, allFriends) => friends
+    .sort((a, b) => (getLevel(a, allFriends) - getLevel(b, allFriends) ||
+    a.name.localeCompare(b.name))
+    );
+
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError('Not instance of filter');
+    }
+    friends.forEach(element =>
+        parents.push({ name: element.name, parent: null })
+    );
+    const stack = sortByLevels(filter.smallFilter(friends), friends).reverse();
+    this.stack = stack;
 }
 
-/**
- * Итератор по друзям с ограничением по кругу
- * @extends Iterator
- * @constructor
- * @param {Object[]} friends
- * @param {Filter} filter
- * @param {Number} maxLevel – максимальный круг друзей
- */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    friends.forEach(element =>
+        parents.push({ name: element.name, parent: null })
+    );
+    const stack = sortByLevels(filter.smallFilter(friends), friends);
+    // sam-1 brad mat-2 ethan-2
+    this.stack = stack.filter(element => getLevel(element, friends) <= maxLevel).reverse();
 }
 
-/**
- * Фильтр друзей
- * @constructor
- */
 function Filter() {
-    console.info('Filter');
+    this.smallFilter = function (friends) {
+        return friends;
+    };
 }
 
-/**
- * Фильтр друзей
- * @extends Filter
- * @constructor
- */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.smallFilter = function (friends) {
+        return Filter.smallFilter(friends, 'gender', 'male');
+    };
 }
 
-/**
- * Фильтр друзей-девушек
- * @extends Filter
- * @constructor
- */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.smallFilter = function (friends) {
+        return Filter.smallFilter(friends, 'gender', 'female');
+    };
 }
+
+Object.setPrototypeOf(FemaleFilter.prototype, Filter.prototype);
+Object.setPrototypeOf(MaleFilter.prototype, Filter.prototype);
+Object.setPrototypeOf(LimitedIterator.prototype, Iterator.prototype);
+
+Object.assign(Filter, {
+    smallFilter(friends, param, value) {
+        return friends.filter(friend => friend[param] === value);
+    }
+});
+
+Iterator.prototype.done = function () {
+    return this.stack.length === 0;
+};
+
+Iterator.prototype.next = function () {
+    if (this.stack.length > 0) {
+        return this.stack.pop();
+    }
+
+    return null;
+};
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
