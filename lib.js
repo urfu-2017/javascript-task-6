0cb23036230beab52e97cd1a2e847a9877f3ba59
whoe <who-e@yandex.ru>
2017-11-14 04:31:26 +0500

    done

--- lib.js
+++ lib.js
@@ -7,8 +7,86 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError('Bad filter');
+    }
+    this.friends = friends;
+    this.filter = filter;
+    this.maxLevel = Number.MAX_SAFE_INTEGER;
 }
+
+Object.assign(Iterator.prototype, {
+    done() {
+        return this.current === this.filtredInvited.length && this.prepared;
+    },
+    next() {
+        if (!this.prepared) {
+            this.prepare(this.friends);
+            this.prepared = true;
+        }
+        if (this.current < this.filtredInvited.length) {
+            return this.filtredInvited[this.current++];
+        }
+
+        return null;
+    },
+    invited: [],
+    filtredInvited: [],
+    prepared: false,
+    current: 0,
+
+    /**
+     * Подготавливает массив filtredInvited для функции next и done
+     * Устанавливает флаг prepared
+     */
+    prepare() {
+        this.prepareNextLevel(friend => friend.best);
+        let level = 2;
+        while (level <= this.maxLevel) {
+            let prepareResult = this.prepareNextLevel(this.friendsOfInvitedFriends);
+            if (!prepareResult) {
+                break;
+            }
+            level++;
+        }
+        if (level <= this.maxLevel) {
+            this.prepareNextLevel(friend => !this.invited.includes(friend));
+        }
+        this.filtredInvited = this.filter.useFilter(this.invited);
+    },
+
+    /**
+     * Принимает функцию, которая определяет следующий круг друзей
+     * Функция должна возвращать true, если друг подходит для следующего круга
+     * Добавляет друзей следующего круга в invited.
+     * Возвращает true, если есть друзья в следующем круге
+     * @param {Function} filterCallback
+     * @returns {Boolean}
+     */
+    prepareNextLevel(filterCallback) {
+        let nextLevelFriends = this.friends.filter(filterCallback, this);
+        if (nextLevelFriends.length === 0) {
+            return false;
+        }
+        nextLevelFriends.sort((a, b) => a.name > b.name);
+        this.invited = this.invited.concat(nextLevelFriends);
+
+        return true;
+    },
+
+    /**
+     * функция для filter
+     * Возвращает true, если друг относится к следующему кругу.
+     * Следующий круг - друзья уже приглашенных друзей
+     * @param {Object} friend 
+     * @returns {Boolean}
+     */
+    friendsOfInvitedFriends(friend) {
+        return !this.invited.includes(friend) && this.friends
+            .filter(friendOther => this.invited.includes(friendOther))
+            .some(invitedFriend => invitedFriend.friends.includes(friend.name));
+    }
+});
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -19,16 +97,29 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    Iterator.call(this, friends, filter);
+    this.maxLevel = maxLevel;
 }
+
+Object.setPrototypeOf(LimitedIterator.prototype, Iterator.prototype);
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    // Nothing
 }
+
+/**
+ * @this
+ */
+Object.assign(Filter.prototype, {
+    condition: () => true,
+    useFilter(friends) {
+        return friends.filter(this.condition);
+    }
+});
 
 /**
  * Фильтр друзей
@@ -36,8 +127,13 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    // Nothing
 }
+
+Object.setPrototypeOf(MaleFilter.prototype, Filter.prototype);
+Object.assign(MaleFilter.prototype, {
+    condition: x => x.gender === 'male'
+});
 
 /**
  * Фильтр друзей-девушек
@@ -45,8 +141,13 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    // Nothing
 }
+
+Object.setPrototypeOf(FemaleFilter.prototype, Filter.prototype);
+Object.assign(FemaleFilter.prototype, {
+    condition: x => x.gender === 'female'
+});
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
