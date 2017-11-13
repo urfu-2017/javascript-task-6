0cbc07b2171e0a79a8270d736dcf709d30c63e11
ilearnf <dj.noon35@gmail.com>
2017-11-13 19:38:07 +0500

    all

--- lib.js
+++ lib.js
@@ -1,5 +1,63 @@
 'use strict';
 
+function defaultSort(depths) {
+    return function (a, b) {
+        if (depths[a.name] < depths[b.name])
+        {
+            return -1;
+        }
+        else if (depths[a.name] > depths[b.name]){
+            return 1;
+        }
+        if (a.name < b.name)
+        {
+            return -1;
+        }
+        else if (a.name > b.name)
+        {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    
+}
+
+function getFriendsDepths(allFriends) {
+    var depths = {};
+    allFriends.forEach(function (friend) {
+        depths[friend.name] = allFriends.length + 1;
+    });
+    var currentDepth = 0;
+    var best = allFriends.filter(function (friend) {
+        return friend.best;
+    }).map(function (friend) {
+        return friend.name;
+    });
+    var visitedNames = [];
+    var nextLevel = [];
+    var currentLevel = [];
+    nextLevel = nextLevel.concat(best);
+    while (nextLevel.length) {
+        currentLevel = nextLevel;
+        currentDepth += 1;
+        nextLevel = [];
+        while (currentLevel.length) {
+            var currentName = currentLevel.shift();
+            depths[currentName] = currentDepth;
+            visitedNames.push(currentName);
+            var currentFriends = allFriends.find(function (friend) {
+                return friend.name === currentName;
+            }).friends.filter(function (friendName) {
+                return !visitedNames.includes(friendName);
+            });
+            nextLevel = nextLevel.concat(currentFriends);
+        }
+    }
+
+    return depths;
+}
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -8,7 +66,35 @@
  */
 function Iterator(friends, filter) {
     console.info(friends, filter);
+    if (!(filter instanceof Filter))
+    {
+        throw new TypeError();
+    }
+    var depths = getFriendsDepths(friends);
+    this.friends = friends.filter(filter.condition)
+        .sort(defaultSort(depths));
+    this.filter = filter;
 }
+Iterator.prototype.currentIndex = 0;
+Iterator.prototype.bestWereIterated = false;
+Iterator.prototype.next = function () {
+    this.currentIndex += 1;
+    if (this.currentIndex > this.friends.length)
+    {
+        return null;
+    }
+
+    return this.friends[this.currentIndex - 1];
+};
+Iterator.prototype.done = function () {
+    var oldIndex = this.currentIndex;
+    var done = this.next() === null;
+    this.currentIndex = oldIndex;
+
+    return done;
+}
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+LimitedIterator.prototype.constructor = Iterator;
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -20,8 +106,13 @@ function Iterator(friends, filter) {
  */
 function LimitedIterator(friends, filter, maxLevel) {
     console.info(friends, filter, maxLevel);
+    var depths = getFriendsDepths(friends);
+    this.friends = friends.filter(function (friend) {
+        return depths[friend.name] <= maxLevel &&
+            filter.condition(friend);
+    }).sort(defaultSort(depths));
+    this.filter = filter;
 }
-
 /**
  * Фильтр друзей
  * @constructor
@@ -30,6 +121,15 @@ function Filter() {
     console.info('Filter');
 }
 
+Filter.prototype.condition = function (item) {
+    return true;
+};
+
+MaleFilter.prototype = Object.create(Filter.prototype);
+MaleFilter.prototype.constructor = MaleFilter;
+FemaleFilter.prototype = Object.create(Filter.prototype);
+FemaleFilter.prototype.constructor = FemaleFilter;
+
 /**
  * Фильтр друзей
  * @extends Filter
@@ -37,6 +137,9 @@ function Filter() {
  */
 function MaleFilter() {
     console.info('MaleFilter');
+    this.condition = function (item) {
+        return item.gender === 'male';
+    };
 }
 
 /**
@@ -46,6 +149,9 @@ function MaleFilter() {
  */
 function FemaleFilter() {
     console.info('FemaleFilter');
+    this.condition = function (item) {
+        return item.gender === 'female';
+    };
 }
 
 exports.Iterator = Iterator;
