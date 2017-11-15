55c090958105470f614c5b088487107dca032809
NurievMikhail <nurievmm@yandex.ru>
2017-11-16 03:29:49 +0500

    Решение задачи

--- lib.js
+++ lib.js
@@ -1,5 +1,43 @@
 'use strict';
 
+function compareFriends(friend1, friend2) {
+    return (friend1.name > friend2.name) ? 1 : -1;
+}
+
+function removeRepetitions(array) {
+    var result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        let currentItem = array[i];
+        if (result.indexOf(currentItem) === -1) {
+            result.push(currentItem);
+        }
+    }
+
+    return result;
+}
+
+function filterFriends(friends, filter, maxLevel = Infinity) {
+    if (maxLevel === 0) {
+        return [];
+    }
+    const bestFriends = friends.filter((friend) => (friend.best)).sort(compareFriends);
+    let resultFriends = [].concat(bestFriends);
+    for (let currentLevel = 1; currentLevel < maxLevel; currentLevel++) {
+        let friendsOfFriendsNames = removeRepetitions(resultFriends
+            .reduce((result, friend) => (result.concat(friend.friends)), []));
+        let friendsOfFriends = friendsOfFriendsNames
+            .map((friendName) => (friends.find((friend) => (friend.name === friendName))))
+            .sort(compareFriends);
+        resultFriends = removeRepetitions(resultFriends.concat(friendsOfFriends));
+        if (friendsOfFriends.length === friends.length) {
+            break;
+        }
+    }
+
+    return removeRepetitions(resultFriends.filter(filter.filter));
+}
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -7,8 +45,26 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError('Вторым аргументом необходимо передать фильтр!');
+    }
+    this.filteredFriends = filterFriends(friends, filter);
+    this.pointer = 0;
 }
+
+Iterator.prototype.done = function () {
+    return this.pointer === this.filteredFriends.length;
+};
+
+Iterator.prototype.next = function () {
+    if (this.done()) {
+        return null;
+    }
+    let nextFtiend = this.filteredFriends[this.pointer];
+    this.pointer++;
+
+    return nextFtiend;
+};
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -19,15 +75,19 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    this.filteredFriends = filterFriends(friends, filter, maxLevel);
+    this.pointer = 0;
 }
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+LimitedIterator.prototype.constructor = LimitedIterator;
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.filter = () => (true);
 }
 
 /**
@@ -36,8 +96,12 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.filter = (friend) => (friend.gender === 'male');
 }
+
+
+MaleFilter.prototype = Object.create(Filter.prototype);
+MaleFilter.prototype.constructor = MaleFilter;
 
 /**
  * Фильтр друзей-девушек
@@ -45,8 +109,12 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.filter = (friend) => (friend.gender === 'female');
 }
+
+FemaleFilter.prototype = Object.create(Filter.prototype);
+FemaleFilter.prototype.constructor = FemaleFilter;
+
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
