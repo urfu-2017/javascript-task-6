3f371e16d903b9265ad27dbbaf291dae62cafc8f
georgii1605@gmail.com <Dtlmvf42>
2017-11-15 20:07:22 +0500

    done

--- lib.js
+++ lib.js
@@ -7,8 +7,17 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError();
+    }
+    this.guestList = getGuestList(friends, filter);
 }
+Iterator.prototype.done = function () {
+    return this.guestList.length === 0;
+};
+Iterator.prototype.next = function () {
+    return this.done() ? null : this.guestList.shift();
+};
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -19,15 +28,17 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    this.guestList = getGuestList(friends, filter, maxLevel);
 }
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+LimitedIterator.prototype.constructor = LimitedIterator;
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.filter = () => true;
 }
 
 /**
@@ -36,8 +47,10 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.filter = friend => friend.gender === 'male';
 }
+MaleFilter.prototype = Object.create(Filter.prototype);
+MaleFilter.prototype.constructor = MaleFilter;
 
 /**
  * Фильтр друзей-девушек
@@ -45,7 +58,39 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.filter = friend => friend.gender === 'female';
+}
+FemaleFilter.prototype = Object.create(Filter.prototype);
+FemaleFilter.prototype.constructor = FemaleFilter;
+
+function compareByName(friend, anotherFriend) {
+    if (friend.name === anotherFriend.name) {
+        return 0;
+    }
+
+    return friend.name > anotherFriend.name ? 1 : -1;
+}
+
+function getGuestList(friends, filter, maxLevel = Infinity) {
+    let guestList = [];
+    let circleOfFriends = friends
+        .filter(friend => friend.best)
+        .sort(compareByName);
+
+    while (circleOfFriends.length && maxLevel--) {
+        guestList = guestList.concat(circleOfFriends);
+        circleOfFriends = getNewCircle(circleOfFriends, friends, guestList);
+    }
+
+    return guestList.filter(friend => filter.filter(friend));
+}
+
+function getNewCircle(currentCircle, friends, guestList) {
+    return currentCircle
+        .reduce((circle, friend) => [...circle, ...friend.friends], [])
+        .map(name => friends.find(friend => friend.name === name))
+        .filter(friend => !guestList.includes(friend))
+        .sort(compareByName);
 }
 
 exports.Iterator = Iterator;
