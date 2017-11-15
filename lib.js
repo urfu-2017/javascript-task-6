132d65adb148f532bee199c893d060e8c2e42c03
HEXBATAET <anton.aka.fly@gmail.com>
2017-11-15 18:31:24 +0500

    done

--- lib.js
+++ lib.js
@@ -1,5 +1,24 @@
 'use strict';
 
+function getInvitedFriends(friends, level = Infinity) {
+    let invitedFriends = [];
+    let stack = friends
+        .filter(friend => friend.best)
+        .sort((a, b) => a.name.localeCompare(b.name));
+    while (level-- > 0 && stack.length > 0) {
+        invitedFriends = invitedFriends.concat(stack);
+        stack = [];
+        stack = invitedFriends
+            .map((invitedFriend, i, arr) => friends
+                .filter(friend => !arr.includes(friend) &&
+                 friend.friends.includes(invitedFriend.name)));
+        stack = stack.reduce((acc, members) => acc.concat(members), []);
+    }
+
+    return invitedFriends;
+}
+
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -7,7 +26,18 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError();
+    }
+    this.index = 0;
+    this.invitedFriends = getInvitedFriends(friends).filter(filter.check);
+
+    this.done = function () {
+        return this.index === this.invitedFriends.length;
+    };
+    this.next = function () {
+        return (this.done()) ? null : this.invitedFriends[this.index++];
+    };
 }
 
 /**
@@ -19,15 +49,18 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    Iterator.call(this, friends, filter);
+    this.invitedFriends = getInvitedFriends(friends, maxLevel).filter(filter.check);
 }
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+LimitedIterator.prototype.constructor = LimitedIterator;
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.check = () => true;
 }
 
 /**
@@ -36,8 +69,10 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.check = friend => friend.gender === 'male';
 }
+MaleFilter.prototype = Object.create(Filter.prototype);
+MaleFilter.prototype.constructor = MaleFilter;
 
 /**
  * Фильтр друзей-девушек
@@ -45,8 +80,10 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.check = friend => friend.gender === 'female';
 }
+FemaleFilter.prototype = Object.create(Filter.prototype);
+FemaleFilter.prototype.constructor = FemaleFilter;
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
