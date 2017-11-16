67a6be9c701a0cecafcacafc8ee98ee6723fc844
Maxim Akulin <maxim.akulin@gmail.com>
2017-11-16 12:08:36 +0500

    some fixes

--- lib.js
+++ lib.js
@@ -1,59 +1,5 @@
 'use strict';
 
-class Iterator {
-    constructor(friends, filter, maxLevel = Infinity) {
-        this.currentIndex = 0;
-        this.collection = this.completeCollection(friends, filter, maxLevel);
-        console.info(this.collection);
-    }
-
-    next() {
-        return this.done() ? null : this.collection[this.currentIndex++];
-    }
-
-    done() {
-        return this.collection.length === this.currentIndex;
-    }
-
-    completeCollection(friends, filter, maxLevel) {
-        let friendsMap = {};
-        friends.forEach(friend => {
-            friendsMap[friend.name] = friend;
-        });
-        let prevFriends = friends.filter(f => f.best)
-            .sort((a, b) => a.name > b.name ? 1 : -1);
-        let invitedFriends = [].concat(prevFriends);
-        maxLevel--;
-        let currentFriends = [];
-        let isVisited = friend => invitedFriends.includes(friend) ||
-            currentFriends.includes(friend);
-        let addNewFriends = friend => {
-            friend.friends.forEach(name => {
-                if (!isVisited(friendsMap[name])) {
-                    currentFriends.push(friendsMap[name]);
-                }
-            });
-        };
-        while (prevFriends.length > 0 && maxLevel-- > 0) {
-            prevFriends.forEach(friend => {
-                addNewFriends(friend);
-            });
-            currentFriends.sort((a, b) => a.name > b.name ? 1 : -1);
-            invitedFriends.push(...currentFriends);
-            prevFriends = currentFriends;
-            currentFriends = [];
-        }
-
-        return invitedFriends.filter(filter.isCorrectObject);
-    }
-}
-
-class LimitedIterator extends Iterator {
-    constructor(friends, filter, maxLevel) {
-        super(friends, filter, maxLevel);
-    }
-}
-
 class Filter {
     isCorrectObject() {
         return true;
@@ -69,6 +15,69 @@ class MaleFilter extends Filter {
 class FemaleFilter extends Filter {
     isCorrectObject(obj) {
         return obj.gender === 'female';
+    }
+}
+
+
+class Iterator {
+    constructor(friends, filter, maxLevel = Infinity) {
+        if (!(filter instanceof Filter)) {
+            throw new TypeError();
+        }
+        this.currentIndex = 0;
+        this.collection = this.completeCollection(friends, filter, maxLevel);
+    }
+
+    next() {
+        return this.done() ? null : this.collection[this.currentIndex++];
+    }
+
+    done() {
+        return this.collection.length === this.currentIndex;
+    }
+
+    completeCollection(friends, filter, maxLevel) {
+        if (!maxLevel) {
+            return [];
+        }
+        let friendsMap = {};
+        friends.forEach(friend => {
+            friendsMap[friend.name] = friend;
+        });
+        let prevFriends = friends.filter(f => f.best)
+            .sort((a, b) => a.name > b.name ? 1 : -1);
+        let invitedFriends = [].concat(prevFriends);
+        maxLevel--;
+        let currentFriends = [];
+        while (prevFriends.length > 0 && maxLevel-- > 0) {
+            this._addNewFriends(prevFriends, friendsMap, invitedFriends, currentFriends);
+            prevFriends = currentFriends;
+            currentFriends = [];
+        }
+
+        return invitedFriends.filter(filter.isCorrectObject);
+    }
+
+    _addNewFriends(prevFriends, friendsMap, invitedFriends, currentFriends) {
+        let isVisited = f => invitedFriends.includes(f) ||
+            currentFriends.includes(f);
+        prevFriends.forEach(friend => {
+            friend.friends.forEach(name => {
+                if (!isVisited(friendsMap[name])) {
+                    currentFriends.push(friendsMap[name]);
+                }
+            });
+        });
+        currentFriends.sort((a, b) => a.name > b.name ? 1 : -1);
+        invitedFriends.push(...currentFriends);
+
+    }
+
+}
+
+class LimitedIterator extends Iterator {
+    constructor(friends, filter, maxLevel) {
+        super(friends, filter, maxLevel);
     }
 }
 
