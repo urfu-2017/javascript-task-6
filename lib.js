3cc8988ceb1b70e1222fdc6e657386d05fe7a4ef
ToshchevIvan <toshchev.ivan@gmail.com>
2017-11-16 15:22:23 +0500

    Excuses And Accusations

--- lib.js
+++ lib.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -7,8 +8,46 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError();
+    }
+    this._filter = filter;
+    this._pointer = 0;
+    this._done = false;
+    this._currentCircleNum = 0;
+    this._circlesGenerator = _getCircles(friends);
+    this._currentCircle = this._getNextCircle();
 }
+
+Iterator.prototype._getNextCircle = function () {
+    this._currentCircleNum += 1;
+    const next = this._circlesGenerator.next();
+    this._done = next.done;
+    if (this._done) {
+        return undefined;
+    }
+
+    return next.value
+        .filter(f => this._filter.satisfiesCondition(f));
+};
+
+Iterator.prototype.done = function () {
+    return this._done;
+};
+
+Iterator.prototype.next = function () {
+    if (this.done()) {
+        return null;
+    }
+    const result = this._currentCircle[this._pointer];
+    this._pointer += 1;
+    if (this._pointer === this._currentCircle.length) {
+        this._currentCircle = this._getNextCircle();
+        this._pointer = 0;
+    }
+
+    return result;
+};
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -19,15 +58,21 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    this._maxLevel = maxLevel;
+    Iterator.call(this, friends, filter);
 }
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+LimitedIterator.prototype.done = function () {
+    return this._done || this._currentCircleNum > this._maxLevel;
+};
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.satisfiesCondition = () => true;
 }
 
 /**
@@ -36,7 +81,8 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    Object.setPrototypeOf(this, Filter.prototype);
+    this.satisfiesCondition = friend => friend.gender === 'male';
 }
 
 /**
@@ -45,7 +91,9 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    Object.setPrototypeOf(this, Filter.prototype);
+    this.satisfiesCondition = friend => friend.gender === 'female';
+
 }
 
 exports.Iterator = Iterator;
@@ -54,3 +102,20 @@ exports.LimitedIterator = LimitedIterator;
 exports.Filter = Filter;
 exports.MaleFilter = MaleFilter;
 exports.FemaleFilter = FemaleFilter;
+
+
+function* _getCircles(friends) {
+    const visited = new Set();
+    const friendsByName = new Map(friends.map(f => [f.name, f]));
+    let circle = friends
+        .filter(f => f.best);
+    while (circle.length) {
+        circle.sort((a, b) => a.name.localeCompare(b.name))
+            .forEach(f => visited.add(f.name));
+        yield circle;
+        circle = circle
+            .reduce((acc, curr) => acc.concat(curr.friends), [])
+            .map(fName => friendsByName.get(fName))
+            .filter(f => !visited.has(f.name));
+    }
+}
