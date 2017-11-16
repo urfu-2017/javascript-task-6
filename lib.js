06a9b216cc110a6335bf6614534004cb48a9cd2c
Александр <sanya.rogov37@gmail.com>
2017-11-16 16:28:30 +0500

    Мелкие правки

--- lib.js
+++ lib.js
@@ -74,7 +74,7 @@ class Iterator {
         let waves = this.friendList.filter(friend => friend.best);
         let i = 0;
         while (waves[i]) {
-            waves[i].friends.sort().forEach(function (name) {
+            waves[i].friends.forEach(function (name) {
                 let friend = this.findFriend(name);
                 let checked = this.isChecked(friend);
                 if (!checked) {
@@ -117,7 +117,7 @@ class LimitedIterator extends Iterator {
         let waves = [...wave];
         let i = 0;
         while (i < maxLevel - 1) {
-            wave.forEach(person => person.friends.sort().forEach(function (name) {
+            wave.forEach(person => person.friends.forEach(function (name) {
                 let friend = this.findFriend(name);
                 let checked = this.isChecked(friend);
                 if (!checked) {
