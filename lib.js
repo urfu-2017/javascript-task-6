d72188fa76010f7594de3c2208a11e5546f0978f
Ганиева Альбина Нажиповна <erfolg1996@mail.ru>
2017-11-21 22:37:01 +0500

    Update lib.js

--- lib.js
+++ lib.js
@@ -14,8 +14,7 @@ function functionCompareByName(friend, friendNext) {
 function onlyConnectedFriends(allFriends) {
     let allFriendsFriends = [];
     allFriends.forEach(function (item) {
-        if (item.friends === undefined || !item.friends.length) {
-        // if (item.friends === undefined || item.friends.indexOf(undefined) !== -1) {
+        if (item.friends === undefined || item.friends.indexOf(undefined) !== -1) {
             throw new TypeError('friends of ungefined');
         }
         allFriendsFriends = allFriendsFriends.concat(Array.from(item.friends));
